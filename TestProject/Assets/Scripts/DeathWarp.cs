12197472c82b7c97d787e8e1d3cfab9b5fa139f7
Joseph Burgos <joeyburgos562@gmail.com>
2018-11-15 15:17:43 -0800

    Game; updated second room; fixed changecolor script to work on change; added trigger to send player to title on end of second room. 11/15/18

---
+++ TestProject/Assets/Scripts/DeathWarp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathWarp : MonoBehaviour
+{
+    private bool inTrigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTrigger)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+}
